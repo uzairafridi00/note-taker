d031c07b4cdbc7bab259e8ee932ad6a48f6f242e
Uzair Afridi <uzairafridi00@gmail.com>
2022-08-04 09:29:29 +0500

    delete and complete functionality added to todo

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -2,6 +2,7 @@ import React, { FC } from "react";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
 import { Todo } from "../model";
+import TodoList from "./TodoList";
 
 type Props = {
   todo: Todo;
@@ -10,18 +11,35 @@ type Props = {
 };
 
 const SingleTodo: FC<Props> = ({ todo, todos, setTodos }) => {
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <>
       <form className="todos__single">
-        <span className="todos__single--text">{todo.todo}</span>
+        {todo.isDone ? (
+          <s className="todos__single--text">{todo.todo}</s>
+        ) : (
+          <span className="todos__single--text">{todo.todo}</span>
+        )}
+
         <div>
           <span className="icon">
             <AiFillEdit />
           </span>
-          <span className="icon">
+          <span className="icon" onClick={() => handleDelete(todo.id)}>
             <AiFillDelete />
           </span>
-          <span className="icon">
+          <span className="icon" onClick={() => handleDone(todo.id)}>
             <MdDone />
           </span>
         </div>
